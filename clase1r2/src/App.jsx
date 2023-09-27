39744294872b5cfe5c585e77513ce556a3d37397
Nicoarratia <nicolas.andres.arratia@gmail.com>
2023-09-27 17:40:24 -0300

    Nav bar y link

--- clase1r2/src/App.jsx
+++ clase1r2/src/App.jsx
@@ -3,15 +3,17 @@ import './App.css'
 import HomePage from './components/HomePage'
 import Contacto from './components/Contacto'
 import NotFound from './components/NotFound'
+import NavBarMenu from './components/NavBarMenu'
 
 import { Route, Routes } from 'react-router-dom'
+
 
 function App() {
   
 
   return (
     <>
-      
+      <NavBarMenu/>
       <Routes>
         <Route path='/' element={<HomePage />}/>
         <Route path='/Contacto' element={<Contacto/>} />
--- clase1r2/src/components/NavBarMenu.jsx
+++ clase1r2/src/components/NavBarMenu.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
 
-const NavBArMEnu = () => {
+const NavBarMenu = () => {
   return (
-    <div>NavBArMEnu</div>
+    <>
+    <Navbar expand="lg" className="bg-body-tertiary">
+          <Nav className="me-auto">
+            <Link to='/'>Home</Link>
+            <Link to='Contacto' >Contacto</Link>  
+            HappyCake
+          </Nav>
+    </Navbar>
+    </>
   )
 }
 
-export default NavBArMEnu
+export default NavBarMenu
