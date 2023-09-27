f6f373e1209b48c0333b956b7c802c0ab9a2ca1b
Nicoarratia <nicolas.andres.arratia@gmail.com>
2023-09-27 20:25:17 -0300

    app.css con boot navbarmenu

--- clase1r2/src/components/NavBarMenu.jsx
+++ clase1r2/src/components/NavBarMenu.jsx
@@ -6,12 +6,31 @@ import { Link } from 'react-router-dom';
 const NavBarMenu = () => {
   return (
     <>
-    <Navbar expand="lg" className="bg-body-tertiary">
-          <Nav className="me-auto">
-            <Link to='/' className='pe-2'>Home</Link>
-            <Link to='Contacto' className='pe-2'>Contacto</Link>  
-            <p>HappyCake</p>
-          </Nav>
+    <Navbar expand="lg" bg="danger" variant="dark">
+      <Nav className="me-auto">
+        
+        <Link to="/" className="nav-link">
+          <span role="img" aria-label="Casa" className="me-2">
+            🏠
+          </span>
+          Home
+        </Link>
+
+        <Link to="/contacto" className="nav-link">
+          <span role="img" aria-label="Libreta" className="me-2">
+            📓
+          </span>
+          Contacto
+        </Link>
+
+      </Nav>
+      <div className="d-flex align-items-center">
+        
+        <p className="mb-0">HappyCake</p>
+        <span role="img" aria-label="Pastel" className="me-2">
+          🍰
+        </span>
+      </div>
     </Navbar>
     </>
   )
