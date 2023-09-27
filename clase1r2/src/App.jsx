50d432e698dabbf3f5c7987c5b249c5642789fa2
Nicoarratia <nicolas.andres.arratia@gmail.com>
2023-09-27 14:08:46 -0300

    orden carpetas

--- clase1r2/src/App.jsx
+++ clase1r2/src/App.jsx
@@ -1,18 +1,22 @@
 
 import './App.css'
-import HomePage from './HomePage'
-import Contacto from './Contacto'
+import HomePage from './components/HomePage'
+import Contacto from './components/Contacto'
+import NotFound from './components/NotFound'
 
 import { Route, Routes } from 'react-router-dom'
+import NotFound from './components/NotFound'
 
 function App() {
   
 
   return (
     <>
+      
       <Routes>
         <Route path='/' element={<HomePage />}/>
         <Route path='/Contacto' element={<Contacto/>} />
+        <Route path='/*' element={<NotFound/>}/>
       </Routes> 
     </>
   )
---
+++ clase1r2/src/components/NavBarMenu.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NavBArMEnu = () => {
+  return (
+    <div>NavBArMEnu</div>
+  )
+}
+
+export default NavBArMEnu
