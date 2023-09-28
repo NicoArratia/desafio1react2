8cb2c2ccdb3ec17c3c856750a253d321b086999a
Nicoarratia <nicolas.andres.arratia@gmail.com>
2023-09-27 21:00:05 -0300

    estilo a formulario de contacto

--- clase1r2/src/components/Contacto.jsx
+++ clase1r2/src/components/Contacto.jsx
@@ -5,18 +5,26 @@ import Button from 'react-bootstrap/Button';
 const Contacto = () => {
   return (
     <>
-    <Form>
-    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-      <Form.Label>Correo</Form.Label>
-      <Form.Control type="email" placeholder="name@example.com" />
-    </Form.Group>
-    <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-      <Form.Label>Descripción</Form.Label>
-      <Form.Control as="textarea" rows={3} />
-    </Form.Group>
-  </Form>
+    <div className="container mt-5"> {/* Agrega un contenedor Bootstrap y un margen superior */}
+      <div className="row justify-content-center">
+        <div className="col-md-6">
+          <Form>
+            <h1 className="text-center">Cuéntanos, ¿En qué te podemos ayudar?</h1>
 
-  <Button variant="danger">Enviar</Button>
+            <Form.Group controlId="exampleForm.ControlInput1">
+              <Form.Label>Correo</Form.Label>
+              <Form.Control type="email" placeholder="name@example.com" />
+            </Form.Group>
+            <Form.Group controlId="exampleForm.ControlTextarea1">
+              <Form.Label>Descripción</Form.Label>
+              <Form.Control as="textarea" rows={3} />
+            </Form.Group>
+            <br />
+            <Button variant="danger" className="d-block mx-auto">Enviar</Button> {/* Centra el botón */}
+          </Form>
+        </div>
+      </div>
+    </div>
   </>
   )
 }
